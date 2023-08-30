8822ccf1c6d5dfe548cabcd7493015be194297b2
doyounii <lsyjyh@gmail.com>
2023-08-30 19:15:49 +0900

    Modify sql

--- src/edu/weekstore/model/DBConnect.java
+++ src/edu/weekstore/model/DBConnect.java
@@ -8,10 +8,16 @@ public interface DBConnect {
     final static String NOTICE_SELECT_ALL = "select * from notice order by no desc";
     final static String NOTICE_SELECT_ONE = "select * from notice where no=?";
     final static String NOTICE_INSERT = "insert into notice values (default,?,?,default,default);";
+    final static String NOTICE_SELECT_RANGE = "select * from (select * from notice order by no desc limit 5 offset ?) as notice";
     final static String NOTICE_UPDATE = "update notice set title=?, content=? where no=?";
     final static String NOTICE_DELETE = "delete from notice where no=?";
-
-    final static String FAQ_SELECT_ALL = "select * from faq order by fno";
+    final static String NOTICE_COUNT = "select count(*) as cnt from notice";
+    final static String NOTICE_COUNT_TITLE = "select count(*) as cnt from notice where title like ?";
+    final static String NOTICE_COUNT_CONTENT = "select count(*) as cnt from notice where content like ?";
+    final static String NOTICE_COUNT_ALL = "select count(*) as cnt from notice where title like ? or content like ?";
+    final static String NOTICE_SELECT_TITLE_RANGE = "select * from (select * from notice where title like ? order by resdate desc limit 5 offset ?) as notice";
+    final static String NOTICE_SELECT_CONTENT_RANGE = "select * from (select * from notice where content like ? order by resdate desc limit 5 offset ?) as notice";
+    final static String NOTICE_SELECT_ALL_RANGE = "select * from (select * from notice where title like ? or content like ? order by resdate desc limit 5 offset ?) as notice";
 
     final static String CUSTOM_SELECT_ALL = "select * from custom order by regdate desc";
     final static String CUSTOM_SELECT_ONE = "select * from custom where id=?";
@@ -23,10 +29,14 @@ public interface DBConnect {
     final static String PRODUCT_SELECT_ALL = "select * from product order by pno";
     final static String PRODUCT_SELECT_CATE = "select * from product where cate=? order by pno";
     final static String PRODUCT_SELECT_ONE = "select * from product where pno=?";
-    final static String PRODUCT_SELECT_NEW = "select * from product order by pno desc limit 5";
-    final static String PRODUCT_SELECT_BEST = "select * from product where pno in (select pno from payment group by pno order by sum(amount) desc limit 5)";
+    final static String PRODUCT_SELECT_NEW = "select * from (select * from product order by pno desc limit 1) as product";
+    //final static String PRODUCT_SELECT_BEST = "select * from (select * from product where pno in (select pno from payment group by pno order by sum(amount) desc limit 1)) as product";
+    final static String PRODUCT_SELECT_BEST = "SELECT * FROM product WHERE pno IN (SELECT pno FROM (SELECT pno, SUM(amount) as total_amount FROM payment GROUP BY pno ORDER BY total_amount DESC LIMIT 1 ) subquery);";
+
     final static String PRODUCT_INSERT = "insert into product values(default, ?, '', ?, ?, ?, ?, ?, ?, ?, default)";
-    final static String PRODUCT_INSERT_UPDATE = "update product set prono = concat(cate, pno) where pno = (select pno from product order by resdate desc limit 1)";
+    //final static String PRODUCT_INSERT_UPDATE = "select * from (update product set prono = concat(cate, pno) where pno in (select pno from product order by resdate desc limit 1)) as product";
+    final static String PRODUCT_INSERT_UPDATE = "UPDATE product SET prono = CONCAT(cate, pno) WHERE pno = (SELECT pno FROM product ORDER BY resdate DESC LIMIT 1)";
+
     final static String PRODUCT_UPDATE = "update product set pname=?, pcomment=?, plist=?, price=?, imgsrc1=?, imgsrc2=?, imgsrc3=? where pno=?";
     final static String PRODUCT_DELETE = "delete from product where pno=?";
 
@@ -36,10 +46,12 @@ public interface DBConnect {
 
     //출고 처리 패턴
     final static String SERVE_PAYMENT = "insert into payment values (default, ?, ?, ?, ?, ?, ?, ?, '')";
-    final static String SERVE_INSERT = "insert into serve values(default, ?, ?, ?, default, default)";
+    final static String SERVE_INSERT = "insert into serve values(default, ?, ?, ?, default, ?)";
+    final static String SERVE_INSERT_RECEIVE = "insert into serve values(default, ?, ?, ?, default, default)";
+
     final static String DELIVERY_INSERT = "insert into delivery values (default, ?, ?, ?, ?, '','',default,default,'','')";
     final static String CART_DELETE = "delete from cart where cartno=?";
-    final static String GET_SNO = "select sno from payment order by sno desc limit 1";
+    final static String GET_SNO = "select * from (select sno from payment order by sno desc limit 1) as payment";
 
     //반품 처리 패턴
     final static String RETURN_PAYMENT = "delete from payment where sno=?";
@@ -49,17 +61,20 @@ public interface DBConnect {
     final static String RETURN_DELEVERY = "delete from delivery where sno=?";
 
     //배송 등록 처리
-    final static String DELIVERY_PRO = "update delivery set pcom=?, ptel=?, pstate=1, sdate=current_timestamp, rdate=?, bcode=? where dno=?";
+    final static String DELIVERY_PRO = "update delivery set pcom=?, ptel=?, pstate=1, rdate=?, bcode=? where dno=?";
 
     //배송 완료 처리
     final static String DELIVERY_COMPLETE = "update delivery set pstate=2 where dno=?";
-
+    final static String DELIVERY_SELECT_ALL = "select * from adminDelivery";
+    final static String DELIVERY_SELECT_LIST = "select * from delivery order by dno desc";
     final static String DELIVERY_SELECT_ONE = "select * from delivery where dno=?";
     final static String DELIVERY_SELECT_SNO = "select * from delivery where sno=?";
+    final static String DELIVERY_SALES_COMPLETE = "update delivery set pstate=3 where dno=?";
 
     //재고 조회
     final static String INVENTORY_SELECT_ALL = "select * from inventory order by pno desc";
-    final static String INVENTORY_SELECT_ONE = "select * from inventory where pno=?";
+    final static String INVENTORY_SELECT_ONE = " select a.pno, (a.amount-b.amount) AS amount from sel1 a, sel2 b  WHERE a.pno =?";
+    //final static String INVENTORY_SELECT_ONE = "select sum(amount) as amount from receive where pno=? group by pno";
 
     //입고 처리
     final static String RECEIVE_INSERT = "insert into receive values (default, ?, ?, ?, default)";
@@ -69,14 +84,25 @@ public interface DBConnect {
     final static String CART_SELECT_CID = "select * from cart where cid=?";
 
     //결제 정보
-    final static String PAYMENT_SELECT_LIST = "select * from payment order by sno desc";
-    final static String PAYMENT_SELECT_ONE = "select * from payment where sno=?";
-    final static String PAYMENT_SELECT_CID = "select * from payment where cid=?";
-    final static String SERVE_INSERT_RECEIVE = "insert into serve values(default, ?, ?, ?, default, default)";
+    final static String PAYMENT_SELECT_LIST ="select * from payment order by sno desc";
+    final static String PAYMENT_SELECT_ONE ="select * from payment where sno=?";
+    final static String PAYMENT_SELECT_CID ="select * from payment where cid=?";
+
+    //QNA
+    final static String QNA_SELECT_ALL = "SELECT * FROM qnalist";
+    final static String QNA_SELECT_ONE = "select * from QNA where qno=?";
+    final static String QNA_INSERT = "insert into qna(title, content, author, lev, par) values(?,?,?,?,?)";
+    final static String QNA_SELECT_RANGE = "select * from (select * from QNA order by qno desc limit 5 offset ?) as qna";
+    final static String QNA_COUNT = "select count(*) as cnt from QNA";
+    final static String QNA_COUNT_TITLE = "select count(*) as cnt from QNA where title like ?";
+    final static String QNA_COUNT_CONTENT = "select count(*) as cnt from QNA where content like ?";
+    final static String QNA_COUNT_ALL = "select count(*) as cnt from QNA where title like ? or content like ?";
+    final static String QNA_SELECT_TITLE_RANGE = "select * from (select * from QNA where title like ? order by resdate desc limit 5 offset ?) as qna";
+    final static String QNA_SELECT_CONTENT_RANGE = "select * from (select * from QNA where content like ? order by resdate desc limit 5 offset ? as qna";
+    final static String QNA_SELECT_ALL_RANGE = "select * from (select * from QNA where title like ? or content like ? order by resdate desc limit 5 offset ?) as qna";
+
 
     public Connection connect();
-
     public void close(PreparedStatement pstmt, Connection conn);
-
     public void close(ResultSet rs, PreparedStatement pstmt, Connection conn);
 }
