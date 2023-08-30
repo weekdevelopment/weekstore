8399e42de8ab2836c7bc7755f7cc3d163649fd63
doyounii <lsyjyh@gmail.com>
2023-08-30 17:26:20 +0900

    Modify delivery

--- src/edu/weekstore/controller/Main.java
+++ src/edu/weekstore/controller/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Main extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String user = "천재 김기태";
+        String user = "천재 doyouni";
 
         ServletContext application = request.getServletContext();
         String realPath = request.getSession().getServletContext().getRealPath("/");
